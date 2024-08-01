0e29e424e681253898a1642b1df4895b3bd52a32
Agenyi Gideon Enemona <139968230+Enesgid@users.noreply.github.com>
2024-08-01 06:25:52 +0100

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,7 @@
+
+function close() {
+    document.getElementById ("sideBar").style.display = 'block';
+}
+// function close() {
+//     document.getElementById ("sideBar").style.display = 'flex' ;
+// }
